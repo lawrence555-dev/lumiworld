b7c54d6fd23a923b350996ec0ef75c3c0db5411a
lawrence <lawrence@lawrencedeMacBook-Air.local>
2026-01-15 08:40:21 +0700

    fix: remove TS types from sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,13 +6,13 @@ const ASSETS = [
     '/favicon.ico',
 ];
 
-self.addEventListener('install', (event: any) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
-self.addEventListener('fetch', (event: any) => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
